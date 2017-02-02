67cdd4813aba67804b5c343a28b3129676aeb7ad
Flugger <flugged@gmail.com>
2017-02-02 20:46:37 +0100

    Fix Echo Echo Echo Echo Echo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
-
 import { MaterialModule } from "@angular/material"
-
-import Echo from "laravel-echo"
-
-
+import * as Echo from 'laravel-echo'
 import 'hammerjs'
+
+console.log(Echo)
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
