d3df864281e1eedda6280ad0264b4687eb048b99
Flugger <flugged@gmail.com>
2017-07-31 00:05:42 +0200

    Fix sales permissions

--- src/app/organization/workspace/project/sales/sales-list/sales-list.component.ts
+++ src/app/organization/workspace/project/sales/sales-list/sales-list.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/combineLatest';
 
 import { SalesListService } from './sales-list.service';
 import { SaleDataSource } from './sale-data-source';
 import { Sale } from '../../../shared/sale.model';
 import { MdDialog, MdDialogConfig } from '@angular/material';
 import { DeleteConfirmationComponent } from '../delete-confirmation/delete-confirmation.component';
+import { ActiveMembershipService } from '../../../active-membership.service';
+import { Membership } from '../../../../shared/membership.model';
 
 @Component({
   providers: [SalesListService],
   templateUrl: 'sales-list.component.html'
 })
-export class SalesListComponent implements OnInit {
+export class SalesListComponent implements OnInit, OnDestroy {
 
   /**
    * Indicates if the component is currently loading.
@@ -22,6 +27,12 @@ export class SalesListComponent implements OnInit {
    */
   sales: Sale[];
 
+
+  /**
+   * The active membership.
+   */
+  membership: Membership;
+
   /**
    * The table data source.
    */
@@ -29,19 +40,35 @@ export class SalesListComponent implements OnInit {
   displayedColumns = ['id'];
 
   /**
+   * List of observable subscriptions.
+   */
+  private subscriptions: Subscription[] = [];
+
+  /**
    * Constructs the component.
    */
   constructor(public salesList: SalesListService,
+              private activeMembership: ActiveMembershipService,
               private dialog: MdDialog) {}
 
   /**
    * Initializes the component.
    */
   ngOnInit() {
-    this.salesList.sales.subscribe(sales => {
-      this.sales = sales;
+    this.subscriptions.push(Observable.combineLatest(
+      this.activeMembership.membership,
+      this.salesList.sales
+    ).subscribe(data => {
+      [this.membership, this.sales] = data;
       this.loading = false;
-    });
+    }));
+  }
+
+  /**
+   * Destroys the component.
+   */
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   /**
@@ -53,5 +80,22 @@ export class SalesListComponent implements OnInit {
         sale: sale
       }
     });
+  }
+
+  /**
+   * Indicates if you can remove a sale.
+   */
+  canRemove(sale) {
+    if (sale.membership.user.isAdmin) {
+      return true;
+    }
+
+    for (const teamMember of this.membership.teamMembers) {
+      if (teamMember.teamId === sale.teamId && teamMember.isLeader) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
