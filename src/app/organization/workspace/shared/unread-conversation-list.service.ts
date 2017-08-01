ba4b925f92e6b6fdcdd088df0ab2cf87943ffec5
Flugger <flugged@gmail.com>
2017-08-01 02:44:33 +0200

    Unread conversations

--- src/app/organization/workspace/shared/unread-conversation-list.service.ts
+++ src/app/organization/workspace/shared/unread-conversation-list.service.ts
@@ -6,6 +6,9 @@ import { ObservableResourceList } from '../../../core/sockets/observable-resourc
 import { SocketApiService } from '../../../core/sockets/socket-api.service';
 import { UnreadConversationService } from '../../shared/unread-conversation.service';
 import { Conversation } from '../messaging/shared/conversation.model';
+import { ActiveUserService } from '../../active-user.service';
+import { Participation } from '../messaging/shared/participation.model';
+import { Message } from '../messaging/shared/message.model';
 
 @Injectable()
 export class UnreadConversationListService extends ObservableResourceList implements OnDestroy {
@@ -19,11 +22,19 @@ export class UnreadConversationListService extends ObservableResourceList implem
    * Constructs the service.
    */
   constructor(private sockets: SocketApiService,
+              private activeUser: ActiveUserService,
               private unreadConversationService: UnreadConversationService) {
     super();
 
     this.paginator.subscribe(limit => {
       this.unreadConversationService.get().subscribe(conversations => this.set(conversations));
+    });
+
+    this.activeUser.user.subscribe(user => {
+      this.sockets.listenForUser(user.id, {
+        'last_message_read': participation => this.updateConversation(participation),
+        'message_sent': message => this.addConversation(message),
+      }, this);
     });
   }
 
@@ -33,5 +44,27 @@ export class UnreadConversationListService extends ObservableResourceList implem
   ngOnDestroy(): void {
     this.sockets.stopListening(this);
     super.ngOnDestroy();
+  }
+
+  /**
+   * Updates the conversation from the given participation.
+   */
+  private updateConversation(participation: Participation) {
+    this.snapshot = this.snapshot.filter(item => item.id !== participation.conversationId);
+
+    this.updateFromSnapshot();
+  }
+
+  /**
+   * Adds a conversation.
+   */
+  private addConversation(message: Message) {
+    const conversation = this.snapshot.find(item => item.id === message.conversationId);
+
+    if (! conversation) {
+      this.snapshot.push(message.conversation);
+
+      this.updateFromSnapshot();
+    }
   }
 }
